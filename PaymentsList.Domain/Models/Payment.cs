7283b80ff860d506c46912ae57cc695146090e35
HighError <hierrorua@gmail.com>
2021-10-17 12:56:37 +0300

    Add User, Group, Payments classes

---
+++ PaymentsList.Domain/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsList.Domain.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public int IssuerId { get; set; }
+        public User Issuer { get; set; }
+        public int RecipientId { get; set; }
+        public User Recipient { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+        public bool IsAccepted {  get; set; }
+
+    }
+}
